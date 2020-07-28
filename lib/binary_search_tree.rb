eb571df82e9cd39d0393d27a9cdc7de8bc73e8a4
Jack Cullen <jpcullen20@gmail.com>
2020-07-28 10:47:03 -0600

    Add include method - only works for trees 3 levels deep

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -12,4 +12,20 @@ class BinarySearchTree
     @root.left.left = node and return 2 if !@root.left.left
     @root.right.right = node and return 2 if !@root.right.right
   end
+
+  def include?(score)
+    return false if !@root
+    return true if check_next_nodes(@root, score)
+    return true if check_next_nodes(@root.left, score)
+    return true if check_next_nodes(@root.right, score)
+    false
+  end
+
+  def check_next_nodes(node, score)
+    left = node.left
+    right = node.right
+    return true if left && left.score == score
+    return true if right && right.score == score
+  end
+
 end
--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -19,4 +19,14 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
   end
 
+  def test_include
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(92, "Sharknado 3")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal true, @tree.include?(16)
+    assert_equal false, @tree.include?(72)
+  end
+
 end
